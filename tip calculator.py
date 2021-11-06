364c946cca23f3068cbadcdb2293348b76a59660
seifbasel <seifbasel950@gmail.com>
2021-11-06 18:55:33 +0200

    enter

--- tip calculator.py
+++ tip calculator.py
@@ -1,6 +1,7 @@
-bill=int(input("enter your bill value:"))
-num_of_persons=int(input("how many people are there to split the bill?"))
-tip_percent=int(input("what percent do you want to pay as tip?"))
+print("tip calculator")
+bill=int(input("enter your bill value:  "))
+num_of_persons=int(input("how many people are there to split the bill?  "))
+tip_percent=int(input("what percent do you want to pay as tip?  "))
 one_should_pay=(bill/num_of_persons)
 tip=((tip_percent/100)*bill)/num_of_persons
 one_will_pay=(tip+one_should_pay)
